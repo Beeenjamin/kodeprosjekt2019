f664312b856546ce68786365ca271b28c3451cb4
Beeenjamin <gamernoob290@gmail.com>
2019-05-24 12:12:55 +0200

    Update buttonhandle.cs

--- buttonhandle.cs
+++ buttonhandle.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//bruker ikke dette scriptet, burde egentlig ha slettet det....
+
 public class buttonhandle : MonoBehaviour
 {
     public void PlayGame() //Lager en fuksjon som vi senere "caller" til for å bruke koden i den.
